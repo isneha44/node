35807ded27f508a6bb2b6bbd91898d08db462ba3
Imalka Gayani Fernando <isneha44@gmail.com>
2024-05-25 06:49:15 +0000

    post And get methods with Express

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require ('express')
+const bodyParser = require ('body-parser')
 const app = express();
 
-app.get('/get',(req,res)=>{
-    res.send('Hello World!')
+//middleware
+app.use(bodyParser.json())
+
+//get
+const data =(req,res)=>{
+    res.send('Hello World!');
+}
+app.get('/get',data)
+
+//post
+app.post('/post',(req,res)=>{
+
+  const {userName,password} = req.body
+  
+res.json({userName,password})
 })
+
 
 app.listen(3000,()=>{
   console.log('service is running port 3000')
